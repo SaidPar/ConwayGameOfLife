088d087a1051a98588dc9d733f943e2218475374
Said Parirokh <thebigrokh@gmail.com>
2019-09-01 23:43:27 -0700

    Initial commit.

---
+++ ConwayGameOfLife/screens/screen_options.c
@@ -0,0 +1,76 @@
+/**********************************************************************************************
+*
+*   raylib - Standard Game template
+*
+*   Options Screen Functions Definitions (Init, Update, Draw, Unload)
+*
+*   Copyright (c) 2014-2019 Ramon Santamaria (@raysan5)
+*
+*   This software is provided "as-is", without any express or implied warranty. In no event
+*   will the authors be held liable for any damages arising from the use of this software.
+*
+*   Permission is granted to anyone to use this software for any purpose, including commercial
+*   applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+*     1. The origin of this software must not be misrepresented; you must not claim that you
+*     wrote the original software. If you use this software in a product, an acknowledgment
+*     in the product documentation would be appreciated but is not required.
+*
+*     2. Altered source versions must be plainly marked as such, and must not be misrepresented
+*     as being the original software.
+*
+*     3. This notice may not be removed or altered from any source distribution.
+*
+**********************************************************************************************/
+
+#include "raylib.h"
+#include "screens.h"
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+
+// Options screen global variables
+static int framesCounter;
+static int finishScreen;
+
+//----------------------------------------------------------------------------------
+// Options Screen Functions Definition
+//----------------------------------------------------------------------------------
+
+// Options Screen Initialization logic
+void InitOptionsScreen(void)
+{
+    // TODO: Initialize OPTIONS screen variables here!
+    framesCounter = 0;
+    finishScreen = 0;
+}
+
+// Options Screen Update logic
+void UpdateOptionsScreen(void)
+{
+	if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+	{
+		finishScreen = 1;   // OPTIONS
+		//finishScreen = 2;   // GAMEPLAY
+	}
+}
+
+// Options Screen Draw logic
+void DrawOptionsScreen(void)
+{
+	DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLACK);
+	DrawText("NEW GAME", 120, 220, 40, WHITE);
+}
+
+// Options Screen Unload logic
+void UnloadOptionsScreen(void)
+{
+    // TODO: Unload OPTIONS screen variables here!
+}
+
+// Options Screen should finish?
+int FinishOptionsScreen(void)
+{
+    return finishScreen;
+}
